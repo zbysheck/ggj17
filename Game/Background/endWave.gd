5a846265810a4cb28502387775511dd544aa4e65
Dawid <d.kubic@komijo.pl>
2017-01-21 16:14:27 +0100

    fix for merge problems

--- Game/Background/endWave.gd
+++ Game/Background/endWave.gd
@@ -5,11 +5,9 @@ func _ready():
 
 func _process(delta):
 	
-<<<<<<< HEAD
+
 	set_pos(Vector2(get_pos().x + 0.67, get_pos().y))
 	get_node("Fala1").play("1")
 	get_node("Fala2").play("1")
 	get_node("Fala3").play("1")
-=======
-	set_pos(Vector2(get_pos().x + (40 * delta), get_pos().y))
->>>>>>> refs/remotes/origin/master
+
