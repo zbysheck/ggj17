8bba82be346010d33ee7036dddaf530fed5ca55d
Patryk Wysocki <wysockipawelpatryk@gmail.com>
2017-01-21 01:24:31 +0100

    surfer

--- Surfer/Surfer.gd
+++ Surfer/Surfer.gd
@@ -1,12 +1,20 @@
 extends RigidBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var CurrentDirectionVector
+
+export(int) var maxSpeed;
 
 func _ready():
 	set_process(true)
 
 func _process(delta):
-	if(Input.is_action_pressed("ui_up")):
-		print("asd")
+	if(Input.is_action_pressed("SURF")):
+		CurrentDirectionVector = Vector2(1, -6)
+		set_gravity_scale(0.05)
+	else:
+		CurrentDirectionVector = Vector2(1, 1)
+		set_gravity_scale(1)
+	set_applied_force(CurrentDirectionVector * get_linear_velocity().length())
+	var velocity = get_linear_velocity().x
+	if(velocity > maxSpeed):
+		set_linear_velocity(Vector2(maxSpeed, get_linear_velocity().y))
