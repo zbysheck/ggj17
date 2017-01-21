76ad3b7101c621306bf10804a4bc3595c9132557
Dawid <d.kubic@komijo.pl>
2017-01-21 10:45:16 +0100

    surfer_movement_animation

--- Surfer/Surfer.gd
+++ Surfer/Surfer.gd
@@ -9,12 +9,27 @@ func _ready():
 
 func _process(delta):
 	if(Input.is_action_pressed("SURF")):
-		CurrentDirectionVector = Vector2(1, -6)
+		CurrentDirectionVector = Vector2(1, -3)
 		set_gravity_scale(0.05)
+		
 	else:
 		CurrentDirectionVector = Vector2(1, 1)
 		set_gravity_scale(1)
 	set_applied_force(CurrentDirectionVector * get_linear_velocity().length())
 	var velocity = get_linear_velocity().x
+	var velocity_y = get_linear_velocity().y
 	if(velocity > maxSpeed):
 		set_linear_velocity(Vector2(maxSpeed, get_linear_velocity().y))
+		
+	var P1 = get_node("Movement")	
+		
+	if (-5 < velocity_y < 5):	
+		P1.play("forward")	
+	elif (velocity_y < -5):	
+		print(str(velocity))
+		P1.play("45-down")
+	elif (velocity_y > 5):
+		print(str(velocity))
+		P1.play("45-up")
+	
+	
