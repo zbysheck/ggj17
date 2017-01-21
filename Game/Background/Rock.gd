1cabae964858e6019a361dcd0f1be6cf027d85e5
Dawid <d.kubic@komijo.pl>
2017-01-21 18:19:23 +0100

    update

--- Game/Background/Rock.gd
+++ Game/Background/Rock.gd
@@ -1,10 +1,5 @@
-extends AnimatedSprite
+extends Area2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+func _on_Rock_body_enter( body ):
+	if body.get_name() == "Surfer":
+		
