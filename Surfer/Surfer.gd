280e2ed0168746ac862c9adb5c37f45ef9a58ba3
Zbyszek Kisły <zbyshekh@gmail.com>
2017-01-21 18:44:22 +0100

    speed fix

--- Surfer/Surfer.gd
+++ Surfer/Surfer.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 var CurrentDirectionVector
 
 var Waves = 0;
+var speed = 0;
 
 export(int) var maxSpeed;
 var isOnTheWave = false;
@@ -11,18 +12,30 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
-	if(Input.is_action_pressed("SURF") && isOnWave()):
-		CurrentDirectionVector = Vector2(1, -3)
-		set_gravity_scale(0.05)
-		
+	maxSpeed = 90
+	if(isOnWave() && !Input.is_action_pressed("SURF")):
+		speed += 0.01
+	if(isOnWave() && Input.is_action_pressed("SURF")):
+		speed -= 0.009
+		CurrentDirectionVector = Vector2(speed, -4)
 	else:
-		CurrentDirectionVector = Vector2(1, 1)
-		set_gravity_scale(1)
+		CurrentDirectionVector = Vector2(speed, 2)
+	if(isOnWave()):
+		set_gravity_scale(0.3)
+	else:
+		set_gravity_scale(2)
 	set_applied_force(CurrentDirectionVector * get_linear_velocity().length())
 	var velocity = get_linear_velocity().x
 	var velocity_y = get_linear_velocity().y
 	if(velocity > maxSpeed):
 		set_linear_velocity(Vector2(maxSpeed, get_linear_velocity().y))
+	if(velocity < 20):
+		set_linear_velocity(Vector2(20, get_linear_velocity().y))
+	print(velocity)
+	if(velocity_y > 1250):
+		set_linear_velocity(Vector2(velocity, 250))
+	if(isOnWave() && velocity_y > 60):
+		set_linear_velocity(Vector2(velocity, 60))
 	
 	var P1 = get_node("Movement")
 	var P1position = P1.get_global_pos()
