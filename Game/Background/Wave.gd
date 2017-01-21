5d519b5de9dff86dc10457d5f827fb7334134318
Patryk Wysocki <wysockipawelpatryk@gmail.com>
2017-01-21 12:02:53 +0100

    błędy kolizji

--- Game/Background/Wave.gd
+++ Game/Background/Wave.gd
@@ -6,23 +6,27 @@ onready var background = get_node("..")
 onready var scene = preload("res://Game/Background/Wave.tscn")
 
 func _ready():
-	set_process(true)
-
-func _process(delta):
 	pass
 
 func _on_Area2D_body_enter( body ):
 	if(body.get_name() == "Surfer"):
 		body.enterWave()
 
-
 func _on_Area2D_body_exit( body ):
 	if(body.get_name() == "Surfer"):
 		body.leaveWave()
 
 func createNextSegment():
+	print("created segment")
 	var newSegment = scene.instance()
-	var segmentPos = newSegment.get_pos()
-	var segmentBegin = newSegment.get_node("BlockBegin")
-	
-	
+	get_node("..").add_child(newSegment)
+	var segmentPos = get_node("BlockEnd").get_pos()
+	var segmentBegin = newSegment.get_node("BlockBegin").get_pos()
+	var xPos = segmentPos.x - segmentBegin.x
+	newSegment.set_global_pos(segmentPos)
+
+func _on_VisibilityNotifier2D_exit_screen():
+	queue_free()
+
+func _on_VisibilityNotifier2D_enter_screen():
+	createNextSegment()
