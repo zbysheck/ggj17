9fb6f47ecfa70777a539ff4f3cd8b35a877cefc4
Patryk Wysocki <wysockipawelpatryk@gmail.com>
2017-01-21 18:49:58 +0100

    ekrany końca gry i tytułowy

---
+++ Game/GameState.gd
@@ -0,0 +1,12 @@
+extends Node
+
+onready var gameOverScene = preload("res://Game/Screens/GameOver.tscn")
+onready var titleScreen = preload("res://Game/Screens/TitleScreen.tscn")
+
+
+func rockHit():
+	get_tree().change_scene("res://Game/Screens/GameOver.tscn")
+func changeToMainScreen():
+	get_tree().change_scene("res://Game/Screens/TitleScreen.tscn")
+func changeToGameScreen():
+	get_tree().change_scene("res://Game/MainScene.tscn")
