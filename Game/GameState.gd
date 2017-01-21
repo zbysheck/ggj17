1c6235bc31a90215a80147a472949a189d0adc10
Patryk Wysocki <wysockipawelpatryk@gmail.com>
2017-01-21 20:18:10 +0100

    pasek turbo działa

--- Game/GameState.gd
+++ Game/GameState.gd
@@ -3,6 +3,8 @@ extends Node
 onready var gameOverScene = preload("res://Game/Screens/GameOver.tscn")
 onready var titleScreen = preload("res://Game/Screens/TitleScreen.tscn")
 
+var points = 0;
+var surfer;
 
 func rockHit():
 	get_tree().change_scene("res://Game/Screens/GameOver.tscn")
